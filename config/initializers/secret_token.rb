53434cc6db8846d8f62d0b86b8c971932cae995f
Joe Susnick <jsusnick@itriagehealth.com>
2014-05-10 15:12:39 -0600

    rails new allisons_cat_palace -d postgresql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AllisonsCatPalace::Application.config.secret_key_base = '9bcc7118c2f738fd8690a25b87f1641386ffea308253a788a8f53e70001d1163e16795892103597c64e5774e6cb5440b45709cb1f69321cc8ef71a18eb97e466'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AllisonsCatPalace::Application.config.session_store :cookie_store, key: '_allisons_cat_palace_session'
