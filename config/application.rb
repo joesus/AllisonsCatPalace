3b4791c48dee7227710ed51d85bb19efc3bc21bf
Joe Susnick <jsusnick@itriagehealth.com>
2014-05-12 19:49:49 -0600

    Really critical line to get app.js.erb to use templates

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ Bundler.require(*Rails.groups)
 
 module AllisonsCatPalace
   class Application < Rails::Application
+
+    config.assets.paths += ["#{Rails.root}/app/assets/templates"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
