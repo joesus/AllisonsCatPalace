bb1097a9327080080f75ad5185ca81c530889036
Joe Susnick <jsusnick@itriagehealth.com>
2014-05-13 16:52:43 -0600

    Refactored rails-side controller and changed auth protocol until I figure out how to use token auth with angular

--- app/controllers/api/kittens_controller.rb
+++ app/controllers/api/kittens_controller.rb
@@ -1,18 +1,46 @@
-module API
+class API::KittensController < ApplicationController
+  respond_to :json
 
-  class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+    respond_with @kittens
+  end
 
-    respond_to :json
+  def show
+    @kitten = Kitten.find(params[:id])
+    respond_with @kitten
+  end
 
-    def index
-      @kittens = Kitten.all
-      respond_with @kittens
-    end
-
-    def show
-      @kitten = Kitten.find(params[:id])
-      respond_with @kitten
+  def create
+    @kitten = Kitten.new(params(kitten_params))
+    if @kitten.save
+      respond_with @kitten, status: :created, location: @kitten
+    else
+      respond_with @kitten.errors, status: :unprocessable_entity
     end
   end
 
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      head 200
+    else
+      respond_with @kitten.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    head :no_content
+  end
+
+  private
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit!
+  end
 end
